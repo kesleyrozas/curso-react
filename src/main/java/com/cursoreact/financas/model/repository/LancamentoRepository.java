d5b09b65da5e55993304707171c6ef7b3799323d
Kesley <kesleyrozas88@gmail.com>
2024-06-19 23:08:36 -0300

    ajuste na consulta de saldo

--- src/main/java/com/cursoreact/financas/model/repository/LancamentoRepository.java
+++ src/main/java/com/cursoreact/financas/model/repository/LancamentoRepository.java
@@ -1,6 +1,7 @@
 package com.cursoreact.financas.model.repository;
 
 import com.cursoreact.financas.model.entity.Lancamento;
+import com.cursoreact.financas.model.enums.StatusLancamento;
 import com.cursoreact.financas.model.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,6 +13,8 @@ import java.math.BigDecimal;
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
     @Query(value = " select sum(l.valor) from Lancamento l join l.usuario u " +
-                   " where u.id = :idUsuario and l.tipo = :tipo group by u " )
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+                   " where u.id = :idUsuario and l.tipo = :tipo and l.status = :status group by u " )
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(@Param("idUsuario") Long idUsuario,
+                                                          @Param("tipo") TipoLancamento tipo,
+                                                          @Param("status") StatusLancamento status);
 }
